2a536d5eaf498ed27ee40252b8381d3c9498ba2a
Micutzuu25 <alexandrucristi25@yahoo.com>
2023-10-19 18:44:30 +0300

    A doua modificare

---
+++ Pop_AlexandruCristian_Lab1/Controllers/IndexController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pop_AlexandruCristian_Lab1.Controllers
+{
+    public class IndexController : Controller
+    {
+        public string Index()
+        {
+            return "Salutare";
+        }
+
+        public string Greet()
+        {
+            return "Hello";
+        }
+
+        public string Indicator (string nume, int age) 
+        {
+           nume = " Alex";
+           age = 10;   
+            return $"My name is{nume} si am varsta de {age}";
+        }
+
+        
+    }
+   
+}
